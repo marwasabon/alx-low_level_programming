636fe7fa6bb036fcac7b1ccbef0b8a1055011ebb
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-15 13:23:23 -0700

    c task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
 #include <string.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
