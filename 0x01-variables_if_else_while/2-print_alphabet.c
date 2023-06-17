9d81120f260f2f82c5bef181a726bb5862ef52db
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-17 05:30:28 -0700

    c variables 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function takes no input parameters and returns an integer value.
+ * It calculates the last digit of a given number and prints it to the console.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
