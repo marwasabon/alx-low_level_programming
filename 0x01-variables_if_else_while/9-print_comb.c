5840052b8b550ffa58c81f8fd3fc7ef67178cc99
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-17 08:26:25 -0700

    c variables 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function takes no input parameters and returns an integer value.
+ * It calculates the last digit of a given number and prints it to the console.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
