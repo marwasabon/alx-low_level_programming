9c0f40d3ea7762a264f2eb161342e20745f64992
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-17 07:28:19 -0700

    c variables 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function takes no input parameters and returns an integer value.
+ * It calculates the last digit of a given number and prints it to the console.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
