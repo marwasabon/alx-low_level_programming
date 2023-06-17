cbcffcbe6156b9df4a5257ec13337ccfde087b6e
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-17 02:47:36 -0700

    c variables 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
+ * main - Entry point for the program
  *
- * main - Entry point
+ * This function takes no input parameters and returns an integer value.
+ * It calculates the last digit of a given number and prints it to the console.
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
+
 int main(void)
 {
 	int n;
@@ -21,9 +24,9 @@ int main(void)
 	last_digit = n % 10;
 
 	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else if (last_digit < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
 	return (0);
