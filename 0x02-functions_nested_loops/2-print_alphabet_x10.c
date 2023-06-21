d695987dfb2804679297a34e0335096d8b80adf4
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-21 03:46:03 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+
+	void print_alphabet(void)
+	{
+		char c;
+
+		int i;
+
+		for (i = 0; i <= 10; i++)
+		{
+
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+
+			_putchar('\n');
+		}
+	}
