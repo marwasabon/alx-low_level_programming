7c1c9dd222fb2b9ddf37f3d4422ed53d751e8fec
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-21 11:54:18 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,14 @@
 	void print_alphabet(void)
 	{
 		char c;
-
+		
 		int i;
 
-		for (i = 0; i <= 10; i++)
-		{
+		for (i = 1; i <=10; i++){
 
 			for (c = 'a'; c <= 'z'; c++)
 			{
 				_putchar(c);
-			}
-
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
