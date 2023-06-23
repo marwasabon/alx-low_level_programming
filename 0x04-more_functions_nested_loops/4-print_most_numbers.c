b6bf69216503db6af4fc44308309fff93e732250
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-23 00:45:15 -0700

    nested task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,26 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
 /**
  * print_most_numbers - returns 1 if char is digit
  *
- *
+ * Return: nothing
  **/
-	void print_most_numbers(void)
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		int i;
-
-		i = 0;
-
-		while (i < 10)
-		{
-			if (i == 2 || i == 4)
-			{
-				i++;
-				continue;
-			}
-			_putchar(i + '0');
-			i++;
-		}
-		_putchar('\n');
+		if ((i == '2') || (i == '4'))
+			continue;
+		_putchar(i);
 	}
+	_putchar('\n');
+}
