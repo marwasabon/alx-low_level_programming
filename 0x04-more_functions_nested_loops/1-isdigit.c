f2fdcf0826add4e970b1606662686829164cc0e3
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-22 23:09:53 -0700

    nested task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *_isdigit - returns 1 if char is digit
+ *
+ * @c: character to be checked if lower case, type int
+ *
+ * Return: 1 if digit, 0 otherwise
+ **/
+	int _isdigit(int c)
+	{
+		int r = _isdigit(c);
+
+		if (r == 0)
+			return (0);
+		else
+			return (1);
+	}
