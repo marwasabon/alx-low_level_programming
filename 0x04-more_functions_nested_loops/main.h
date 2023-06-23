2c4adf822406438150aff737418d24d9ba7138b7
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-23 00:51:31 -0700

    nested task4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _isupper(int c);
 int _putchar(char);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(void)
+void print_most_numbers(void);
 #endif /* MAIN_H */
