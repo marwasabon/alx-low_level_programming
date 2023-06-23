70fe297d53e50038ac5b1efbe194c255c4b8c445
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-22 23:22:11 -0700

    nested task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *mul - returns multiplication of 2 numbers
+ *
+ * @n1: first num, type int
+ * @n2: first num, type int
+ *
+ * Return: multiplication
+ **/
+	int mul(int n1, int n2)
+	{
+		return n1*n2;
+	}
