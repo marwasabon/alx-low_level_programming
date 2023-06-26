13d1c16fb3159cf82834afd5ab964c33e55c4922
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-26 14:51:08 -0700

    pointers task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - derefencing pointers
+ * @s: pointer to character.
+ * Return: result of swap
+ */
+	int _strlen(char *s)
+{
+	int result = strlen(s);
+
+	return (result);
+}
