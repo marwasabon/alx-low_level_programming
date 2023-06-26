392a2691fa04086bed3102cff069fe04ce09f7ce
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-26 15:07:08 -0700

    pointers task4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - derefencing pointers
+ * @str: pointer to integer.
+ * Return: nothing
+ */
+	void _puts(char *str)
+{
+	int i=0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
