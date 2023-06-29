faa4fd5921a61564acb6b7f668dd644761606a6a
Marwa sabon <Mmarwasabon@gmail.com>
2023-06-29 05:16:11 -0700

    pointers & strings task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @src: second string to copy from
+ * @dest: string to be overwritten
+ * @n: number of vlaues to be concatinate
+ * Description: concatenates src to end of dest
+ * Return: pointer to dest
+ **/
+	char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (dest[i - 1] != '\0')
+		dest[i] = '\0';
+	return (dest);
+}
