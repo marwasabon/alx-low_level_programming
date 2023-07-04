ff56bb39e1375931eb6e58ac065fbec85141c273
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-03 23:13:26 -0700

    pointers arrays task0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+int _putchar(char);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
+#endif /* MAIN_H */
