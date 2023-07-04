dcddfdd6d38b990a9daac0cc9f54edb19a12b458
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-04 03:17:45 -0700

    pointers strings task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strspn - get length of a prefix substring
@@ -9,19 +9,19 @@
  **/
 	unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, exit;
+	int i, j, count;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		exit = 0;
+		count = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
-				exit = 1;
+				count = 1;
 			}
 		}
-		if (exit == 0)
+		if (count == 0)
 		{
 			return (i);
 		}
