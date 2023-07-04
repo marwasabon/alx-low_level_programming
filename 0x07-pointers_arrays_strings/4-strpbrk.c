d422f3b03b457a66d3395bd8931f290beee25368
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-04 04:06:10 -0700

    pointers strings task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept in s
+ * Return: bytes required to find accept
+ **/
+	char *_strpbrk(char *s, char *accept)
+{
+	int i, j, exit;
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (&s[i]);
+}
