7f94efb2722b6d99dcdf0d3bed045caf177b8b7c
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-03 23:36:20 -0700

    pointers arrays task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *_memcpy - fills memory with a constant byte
+ * _memcpy - fills memory with a constant byte
  * @src: pointer to memory
  * @dest: constant byte used to replace
  * @n: number of  bytes to replace
- * Description: fills memory s with byte b for n bytes
+ * Description:copy src to dest  with byte b for n bytes
  * Return: pointer to memory
  **/
 	char *_memcpy(char *dest, char *src, unsigned int n)
