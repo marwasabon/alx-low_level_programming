5994edff3368c06d4ec9e631143823d59a00b79d
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-05 03:19:12 -0700

    pointers & strings task2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 #endif /* MAIN_H */
