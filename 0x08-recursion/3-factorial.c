1a71272ba946d6b77c0a3abf7ff834f81ea76da3
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-08 09:51:20 -0700

    (File)
    recursion task2

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * factorial - find factorial n
+ * @n: given number
+ * Description: find factorial of n using recursion
+ * Return: factorial of n
+ **/
+
+	int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	{
+		n = factorial(n - 1) * n;
+		return (n);
+	}
+}
