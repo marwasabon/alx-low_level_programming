1ac84f66f70d85a7c2c29b46816542d992f0f212
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-10 14:01:41 -0700

    malloc_free task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: grid.
+ */
+	int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (int j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -24,5 +24,6 @@ char *_strstr(char *haystack, char *needle);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
 #endif
 
