793504b3b7ea1edc15bff308af2f23f29ebcadd6
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-10 07:44:08 -0700

    malloc_free task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,11 @@
 		length++;
 	}
 	duplicate = (char *)malloc((length + 1) * sizeof(char));
-
+        
+	if (str == NULL)
+        {
+                return (NULL);
+        }	
 	if (duplicate == NULL)
 	{
 		return (NULL);
