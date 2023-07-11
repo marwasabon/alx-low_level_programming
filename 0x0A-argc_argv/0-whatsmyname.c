9d3bbe16ebccd36cca91ed67b6b3cf426cacdd2b
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-10 22:26:09 -0700

    argc_argv task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * @argc: first argument aka counter.
- * @argv argment vector
+ * @argv: argment vector
  * Return: Always 0 (Success)
  *
  */
@@ -14,11 +14,6 @@
 	 printf("%s\n", argv[0]);
 
 	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-	{
-		printf("argv[%d] = %s\n", count, argv[count]);
-	}
-	}
+		printf("%s\n", argv[0]);
 	return (0);
 }
