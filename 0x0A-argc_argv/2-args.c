24dc3fac36c916b4ffd38624850f9243afc2f51a
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-10 23:34:49 -0700

    argc_argv task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument aka counter.
+ * @argv: argment vector
+ * Return: Always 0 (Success)
+ *
+ */
+	int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	else
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
