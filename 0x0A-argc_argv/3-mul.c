3f97938dc30c69bbb8b9e15bb02a06e2a89982df
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-10 23:56:31 -0700

    argc_argv task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: first argument aka counter.
+ * @argv: argment vector
+ * Return: Always 0 (Success)
+ *
+ */
+	int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int num = atoi(argv[1]);
+
+		int num2 = atoi(argv[2]);
+
+		printf("%d\n", num * num2);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
