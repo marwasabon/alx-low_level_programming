c5a4d81de8113495d32eb90f54a18f894b9ed3b9
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-13 02:08:39 -0700

    more_malloc_task1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -27,4 +27,5 @@ char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
