7a4aa8a4336e86f09cdfbee1edcfd84f11af0fca
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-14 01:47:08 -0700

    more_argc_argv task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,13 +7,13 @@
  * Description: returns the length of a given string
  * Return: length of string as int
  **/
-	int _strlen(char *s)
+int _strlen(char *s)
 {	unsigned int i;
 
 	if (s == NULL)
 		return (1);
 	for (i = 0; s[i] != '\0'; i++)
-	;
+		;
 	return (i + 1);
 }
 /**
@@ -24,7 +24,7 @@
  * Description: concatenates n number of values from src to end of dest
  * Return: pointer to dest
  **/
-	char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *dest;
 	unsigned int size1, size2, i, j;
@@ -33,8 +33,14 @@
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
+	size1 = 0;
+
+	while (s1[size1] != '\0')
+		size1++;
+	size2 = 0;
+
+	while (s2[size2] != '\0')
+		size2++;
 	if (n > size2 - 1)
 		n = size2 - 1;
 
