066fc2b0688442510705912e744ecd9c0977f1eb
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-14 02:59:13 -0700

    more_argc_argv task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,18 +12,22 @@
 int *array_range(int min, int max)
 {
 	int i, *arr;
-    if (min > max) {
-        return (NULL);
-    }
 
-    arr = malloc((max - min + 1) * sizeof(int));
-    if (arr == NULL) {
-        return (NULL);
-    }
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc((max - min + 1) * sizeof(int));
 
-    for (i = 0; i <= max - min; i++) {
-        arr[i] = min + i;
-    }
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 
-    return (arr);
+	for (i = 0; i <= max - min; i++)
+	{
+		arr[i] = min + i;
+	}
+
+	return (arr);
 }
