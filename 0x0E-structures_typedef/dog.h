a07ff224e530ea97f31709c6f68ff031abf89f1c
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-17 12:52:47 -0700

    structures_typedef task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ **/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
