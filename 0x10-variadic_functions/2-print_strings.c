2e3028a312b34a4a1ca39ea2f747409c5a0e8663
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-21 02:18:36 -0700

    variadic_functions task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - orint strings
+ * @separator: name of dog
+ * @n:
+ **/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	const char *c;
+	va_list ap;
+
+	va_start(ap, n);
+
+	c = separator;
+
+	separator = c;
+
+	for (i = 0; i < n; i++)
+	{
+		const char *arg = va_arg(ap, const char *);
+
+
+		if (separator != NULL && i > 0)
+			printf("%s", separator);
+
+		printf("%s", arg == NULL ? "(nil)" : arg);
+
+	}
+	va_end(ap);
+	printf("\n");
+}
