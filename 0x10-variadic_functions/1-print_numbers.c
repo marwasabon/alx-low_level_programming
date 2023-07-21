7802e9a90223d96fd45e1fddd6f518f133417831
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-21 01:11:26 -0700

    variadic_functions task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		if  (separator == NULL)
-		printf("%d ", va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 		else  if (i != n - 1)
 		printf("%s", separator);
 	}
