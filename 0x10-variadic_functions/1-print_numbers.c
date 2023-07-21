ee1004b83003596eb7ee7eb49719a279047a698e
Marwa sabon <Mmarwasabon@gmail.com>
2023-07-21 00:51:25 -0700

    variadic_functions task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - dog object
+ * @separator: name of dog
+ * @n:
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	const char *c;
+	va_list ap;
+
+	va_start(ap, n);
+
+	c = ",";
+
+	separator = c;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1)
+		{
+			if (separator == NULL)
+				return;
+			printf("%s", separator);
+		}
+	}
+	va_end(ap);
+	printf ("\n");
+}
