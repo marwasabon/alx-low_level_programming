095b985eb7e55717648b272a8a274ffb8a1e55ae
MarwaSabon <marwasabon@gmail.com>
2023-07-27 21:44:28 +0000

    singly_linked_lists_task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,20 +6,20 @@
  * Return: length of list.
  */
 	list_t *
-add_node (list_t ** head, const char *str)
+add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	int length;
+
 	length = 0;
 
-
-	new_node = malloc (sizeof (list_t));
+	new_node = malloc(sizeof(list_t));
 
 	while (str[length])
 		length++;
 	new_node->len = length;
 
-	new_node->str = strdup (str);
+	new_node->str = strdup(str);
 	if (!new_node)
 		new_node->next = NULL;
 	else
