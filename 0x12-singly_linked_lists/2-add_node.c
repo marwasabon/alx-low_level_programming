32a75de7cbc3ea7ba63f93698791ff9ecf20028a
MarwaSabon <marwasabon@gmail.com>
2023-07-27 21:41:11 +0000

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node - prints all the elements of a list_t list.
+ * @head: pointer the list.
+ * @str: string to be added.
+ * Return: length of list.
+ */
+	list_t *
+add_node (list_t ** head, const char *str)
+{
+	list_t *new_node;
+	int length;
+	length = 0;
+
+
+	new_node = malloc (sizeof (list_t));
+
+	while (str[length])
+		length++;
+	new_node->len = length;
+
+	new_node->str = strdup (str);
+	if (!new_node)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+
+
+}
