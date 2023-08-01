8c9d84d46c2a048c75f49b7df4a3e7b56d0a391b
Marwa_Sabon <marwasabon@gmail.com>
2023-08-01 04:50:05 -0700

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *current;
 
-	listint_t *new_node
+	listint_t *new_node;
 
 	if (*head == NULL)
 	{
@@ -31,4 +31,49 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	current->next = new_node;
 
 	return (new_node);
+}
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer the list.
+ * @n: numbers to be added
+ * Return: length of list.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+        listint_t *new_node;
+
+        new_node = create_node(n);
+
+        if (new_node == NULL)
+        {
+                return (NULL);
+        }
+
+        new_node->next = *head;
+        *head = new_node;
+
+        return (new_node);
+}
+/**
+ * create_node - creates a int of  linked listint_t list.
+ * @num: number to be added to  the list.
+ * Return: a list.
+ */
+listint_t *create_node(const int num)
+{
+        listint_t *new_node;
+
+        new_node = malloc(sizeof(listint_t));
+
+        if (new_node == NULL)
+        {
+                return (NULL);
+        }
+
+        new_node->n = num;
+        new_node->next = NULL;
+
+        return (new_node);
 }
