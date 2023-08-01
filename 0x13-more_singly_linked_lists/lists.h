62f206e7a956d00035a43b63739b59b8a3a78db4
Marwa_Sabon <marwasabon@gmail.com>
2023-08-01 04:23:37 -0700

    task0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
@@ -22,7 +21,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *create_node (const int num);
+listint_t *create_node(const int num);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
