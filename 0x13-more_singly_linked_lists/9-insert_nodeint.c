2ee9444eddaf12f52e86c8795e8527d0c422fa31
Marwa_Sabon <marwasabon@gmail.com>
2023-08-01 11:57:04 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (current != NULL && count < idx)
 	{
 		prev = current;
