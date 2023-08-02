1040de71f2d3f98af58212297e287c708e425f72
Marwa_Sabon <marwasabon@gmail.com>
2023-08-02 12:00:04 -0700

    task14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - print all the elements of a list_t list.
+ * @head: pointer the list.
+ * Return: length of list.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+
+{
+	listint_t *slow, *fast, *temp;
+
+	slow = head;
+	fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
