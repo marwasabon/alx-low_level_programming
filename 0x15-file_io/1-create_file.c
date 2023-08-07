4ee1e31af9397d6f74e88437f77d4a7470b035e7
Marwa_Sabon <marwasabon@gmail.com>
2023-08-07 14:35:36 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,16 +6,16 @@
 /**
  *  create_file -  creates a file.
  * @filename: test file to be read.
- * @text_content:  text_content is a NULL terminated string to write to the file
+ * @text_content:  text_content is a NULL.
  * Return:  1 on success, -1 on failure.
  */
 
 	int
-create_file (const char *filename, char *text_content)
+create_file(const char *filename, char *text_content)
 {
 	FILE *file;
 
-	file = fopen (filename, "w");
+	file = fopen(filename, "w");
 
 	if (file == NULL)
 	{
@@ -23,14 +23,14 @@ create_file (const char *filename, char *text_content)
 	}
 	if (!text_content)
 	{
-		file = fopen (filename, "w");
+		file = fopen(filename, "w");
 	}
 
-	if (fputs (text_content, file) == EOF)
+	if (fputs(text_content, file) == EOF)
 	{
-		fclose (file);
+		fclose(file);
 		return (-1);
 	}
-	fclose (file);
+	fclose(file);
 	return (1);
 }
