ba601bfd6e18b88be7f4daf97b57860848f39863
Marwa_Sabon <marwasabon@gmail.com>
2023-08-07 15:22:59 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,10 +12,12 @@
 	int
 append_text_to_file(const char *filename, char *text_content)
 {
+	FILE *file;
+
 	if (filename == NULL)
 		return (-1);
 
-	FILE *file = fopen(filename, "w");
+	file = fopen(filename, "w");
 
 	if (file == NULL)
 		return (-1);
@@ -29,7 +31,7 @@ append_text_to_file(const char *filename, char *text_content)
 			fclose(file);
 			return (1);
 		}
-		fclose(file);
-		return (-1);
 	}
+	fclose(file);
+	return (-1);
 }
