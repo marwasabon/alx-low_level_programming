5f00114edcea48b00de004b98d4fa31ab82ac943
Marwa_Sabon <marwasabon@gmail.com>
2023-08-09 06:51:43 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
-
-#include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
-
-
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 /**
  * 
  * append_text_to_file -  append tect to a file.
@@ -12,8 +12,71 @@
  * @text_content:  text_content is a NULL terminated string to write to the file
  * Return:  1 on success, -1 on failure.
  */
-void copy_file(const char *file_from, const char *file_to)
+	void
+copy_file(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
+
+	int fd1, fd2;
+	FILE *file1;
+	char buffer [1024];
+	ssize_t n;
+
+
+	if (file_from == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", file_from);
+	exit (98);
+	file1 = fopen(file_from, "r");
+	if (file1 == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n,", file_from);
+	exit (98);
+
+
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC| S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH );
+	if (fd2 == -1)
+	{
+
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file_to);
+		exit (99);
+	}
+
+
+	n = read(fd1, buffer, 1024);
+	while (n > 0) {
+
+		if (write(fd2, buffer, n) != n) {
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+
+		}
+
+	}
+	if (close(fd1) == -1) {
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1) {              
+		dprintf (STDERR_FILENO, "Error: Can't close fd %d\n", fd2);   
+		exit(100); 
+	} 
+
+
+}
+
+	int
+main (int argc, char *argv[])
+{
+	const char *file_from, *file_to;
+
+	if (argc != 3)
+	{
+		dprintf (2, "Usage: cp file_from file_to");
+		exit (97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	copy_file (file_from, file_to);
+
+	return EXIT_SUCCESS;
 }
