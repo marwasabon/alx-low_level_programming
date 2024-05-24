903bb9bd5edce50b5a967ca1a2d62ab9cae4f25d
marwasabon <marwasabon@gmail.com>
2024-05-24 07:27:06 +0000

    task 100

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef _SEARCH_ALGORTHMS_H_
 #define _SEARCH_ALGORTHMS_H_
-
+#include <stdio.h>
+#include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
